5831225271639db1764add853db2f0ec12768338
Mikko Suni <msoft@hotmail.fi>
2020-01-06 15:13:05 +0200

    rpc test

--- cloud/payment.js
+++ cloud/payment.js
@@ -1,4 +1,5 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
+const JsonRPC = require('simple-jsonrpc-js');
 
 exports.myDateTime = function () {
   return Date();
@@ -8,7 +9,23 @@ exports.startWS = function () {
   console.log('>> startWS');
   const ws = new WebSocket('wss://fierce-shelf-80455.herokuapp.com');
 
+  ////////////////////////////////////////////
+  var jrpc = new JsonRPC();
+  ws.jrpc = jrpc;
+
+  
+
   ws.on('message', function incoming(data) {
   	  console.log(data);
+
+  	  jrpc.call('mul', {y: 3, x: 2}).then(function (result) {
+           console.log('>> mul result: ' + result);
+      });
   });
+
+  ws.jrpc.toStream = function(message){
+    ws.send(message);
+  }
+  ///////////////////////////////////////////
+
 };
