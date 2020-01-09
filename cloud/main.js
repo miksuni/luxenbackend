40f4cdc0cc832734c26d835ebd3c2c1946f602c8
Mikko Suni <msoft@hotmail.fi>
2020-01-09 18:23:22 +0200

    Added saveReceipt function

--- cloud/main.js
+++ cloud/main.js
@@ -170,3 +170,65 @@ Parse.Cloud.define('removeproduct', async (req) => {
 	}
 	return returnMessage;
 });
+
+Parse.Cloud.define('saveReceipt', async (req) => {
+
+	let returnMessage = 'Ok';
+
+	if (Object.keys(req.params).length > 0) {
+		console.log(">> saveReceipt: json contains data");
+
+		var obj = new Parse.Object('Receipt');
+
+		if ('receiptNr' in req.params) {
+			console.log('>>' + req.params.receiptNr);
+			obj.set('receiptNr', req.params.receiptNr);
+		}
+		obj.set('date', new Date());
+		if ('cashier' in req.params) {
+			console.log('>>' + req.params.cashier);
+			obj.set('cashier', req.params.cashier);
+		}
+		if ('totalSum' in req.params) {
+			console.log('>>' + req.params.totalSum);
+			obj.set('totalSum', parseInt(req.params.totalSum, 10));
+		}
+		if ('paymentMethod1' in req.params) {
+			console.log('>>' + req.params.paymentMethod1);
+			const query = new Parse.Query('PaymentMethod');
+			const results = await query.find();
+			var i = 0;
+			for (i; i < results.length; i++) {
+				console.log('>> iterate payment methods...');
+				var n = results[i].id.localeCompare(req.params.paymentMethod1);
+				if (n == 0) {
+					console.log('>> paymentMethod1 found');
+					//obj.set('paymentMethod1', results[i]);
+					obj.set('paymentMethod1Fi', results[i].methodFi);
+					break;
+				}
+			}			
+		}
+		if ('paymentMethod2' in req.params) {
+			console.log('>>' + req.params.paymentMethod1);
+			const query = new Parse.Query('PaymentMethod');
+			const results = await query.find();
+			var i = 0;
+			for (i; i < results.length; i++) {
+				console.log('>> iterate payment methods...');
+				var n = results[i].id.localeCompare(req.params.paymentMethod1);
+				if (n == 0) {
+					console.log('>> paymentMethod2 found');
+					//obj.set('paymentMethod2', results[i]);
+					obj.set('paymentMethod2Fi', results[i].methodFi);
+					break;
+				}
+			}			
+		}
+		obj.save().then(function(obj) {
+			console.log('>> Receipt saved');
+		}, function(err) { console.log(err); });
+	} else {
+		console.log(">> Receipt json does not contain data");
+	}
+});
