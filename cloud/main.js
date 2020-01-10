49ed507889a4eb13de292a3608fd72a7cb1e0564
Mikko Suni <msoft@hotmail.fi>
2020-01-10 18:44:10 +0200

    refactoring receipt saving

--- cloud/main.js
+++ cloud/main.js
@@ -178,29 +178,29 @@ Parse.Cloud.define('saveReceipt', async (req) => {
 	if (Object.keys(req.params).length > 0) {
 		console.log(">> saveReceipt: json contains data");
 
-		/*var obj = new Parse.Object('Receipt');
+		var obj = new Parse.Object('Receipt');
 
-		if ('receiptNr' in req.params) {
-			console.log('>>' + req.params.receiptNr);
-			obj.set('receiptNr', req.params.receiptNr);
+		if ('receiptNr' in req.params.total) {
+			console.log('>>' + req.params.total.receiptNr);
+			obj.set('receiptNr', req.params.total.receiptNr);
 		}
 		obj.set('date', new Date());
-		if ('cashier' in req.params) {
-			console.log('>>' + req.params.cashier);
-			obj.set('cashier', req.params.cashier);
+		if ('cashier' in req.params.total) {
+			console.log('>>' + req.params.total.cashier);
+			obj.set('cashier', req.params.total.cashier);
 		}
-		if ('totalSum' in req.params) {
-			console.log('>>' + req.params.totalSum);
-			obj.set('totalSum', parseInt(req.params.totalSum, 10));
+		if ('totalSum' in req.params.total) {
+			console.log('>>' + req.params.total.totalSum);
+			obj.set('totalSum', parseInt(req.params.total.totalSum, 10));
 		}
-		if ('paymentMethod1' in req.params) {
-			console.log('>>' + req.params.paymentMethod1);
+		if ('paymentMethod1' in req.params.total) {
+			console.log('>>' + req.params.total.paymentMethod1);
 			const query = new Parse.Query('PaymentMethod');
 			const results = await query.find();
 			var i = 0;
 			for (i; i < results.length; i++) {
 				console.log('>> iterate payment methods...');
-				var n = results[i].id.localeCompare(req.params.paymentMethod1);
+				var n = results[i].id.localeCompare(req.params.total.paymentMethod1);
 				if (n == 0) {
 					console.log('>> paymentMethod1 found');
 					//obj.set('paymentMethod1', results[i]);
@@ -209,14 +209,14 @@ Parse.Cloud.define('saveReceipt', async (req) => {
 				}
 			}			
 		}
-		if ('paymentMethod2' in req.params) {
-			console.log('>>' + req.params.paymentMethod1);
+		if ('paymentMethod2' in req.params.total) {
+			console.log('>>' + req.params.total.paymentMethod1);
 			const query = new Parse.Query('PaymentMethod');
 			const results = await query.find();
 			var i = 0;
 			for (i; i < results.length; i++) {
 				console.log('>> iterate payment methods...');
-				var n = results[i].id.localeCompare(req.params.paymentMethod1);
+				var n = results[i].id.localeCompare(req.params.total.paymentMethod1);
 				if (n == 0) {
 					console.log('>> paymentMethod2 found');
 					//obj.set('paymentMethod2', results[i]);
@@ -227,7 +227,7 @@ Parse.Cloud.define('saveReceipt', async (req) => {
 		}
 		obj.save().then(function(obj) {
 			console.log('>> Receipt saved');
-		}, function(err) { console.log(err); });*/
+		}, function(err) { console.log(err); });
 	} else {
 		console.log(">> Receipt json does not contain data");
 	}
