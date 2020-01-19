932892babe6047ab61b2bea7358471fd8f96c8e6
Mikko Suni <msoft@hotmail.fi>
2020-01-19 21:30:12 +0200

    Fix

--- cloud/main.js
+++ cloud/main.js
@@ -42,18 +42,21 @@ Parse.Cloud.define('send_email', async (req) => {
 	 * @param {function} callback The callback to call with the authorized client.
 	 */
 	function authorize(credentials, callback) {
-	  const {client_secret, client_id, redirect_uris} = credentials.installed;
-	  const oAuth2Client = new google.auth.OAuth2(
-	      client_id, client_secret, redirect_uris[0]);
+		  const {client_secret, client_id, redirect_uris} = credentials.installed;
+		  const oAuth2Client = new google.auth.OAuth2(
+		      client_id, client_secret, redirect_uris[0]);
 
-	  // Check if we have previously stored a token.
-	  //fs.readFile(TOKEN_PATH, (err, token) => {
-	  //  if (err) return getNewToken(oAuth2Client, callback);
-	    oAuth2Client.setCredentials(JSON.parse(process.env.GOOGLE_TOKEN));
-	    callback(oAuth2Client);
-	  });
-	}
+		  // Check if we have previously stored a token.
+		  //fs.readFile(TOKEN_PATH, (err, token) => {
+		  //  if (err) return getNewToken(oAuth2Client, callback);
+		  //  oAuth2Client.setCredentials(JSON.parse(token));
+		  //  callback(oAuth2Client);
+		  //});
+		  oAuth2Client.setCredentials(JSON.parse(process.env.GOOGLE_TOKEN));
+		  callback(oAuth2Client);
+		}
 
+	
 	/**
 	 * Get and store new token after prompting for user authorization, and then
 	 * execute the given callback with the authorized OAuth2 client.
