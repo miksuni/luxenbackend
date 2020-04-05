d3946e6ff0543248aed6b50dda995b8be1aa523b
Mikko Suni <msoft@hotmail.fi>
2020-04-05 10:46:34 +0300

    Initial version with real TP

--- cloud/payment.js
+++ cloud/payment.js
@@ -1,7 +1,12 @@
 const WebSocket = require('ws');
 const JsonRPC = require('simple-jsonrpc-js');
 
-var jrpc = new JsonRPC();
+const jrpc = new JsonRPC();
+
+const TERMINALINFO = 1;
+const STATUS = 2;
+const PURCHASE = 3;
+var command = 0;
 
 exports.myDateTime = function () {
   return Date();
@@ -9,20 +14,20 @@ exports.myDateTime = function () {
 
 exports.startWS = function () {
   console.log('>> startWS');
-  const ws = new WebSocket('wss://fierce-shelf-80455.herokuapp.com');
+  //const ws = new WebSocket('wss://fierce-shelf-80455.herokuapp.com');
+  const ws = new WebSocket('wss://' +
+                            process.env.TP_USER +
+                            ':' +
+                            process.env.TP_PASS +
+                            '@api.poplatek.com/api/v2/terminal/' +
+                            process.env.TERMINAL_ID +
+                            '/jsonpos');
 
-  ////////////////////////////////////////////
-  //var jrpc = new JsonRPC();
   ws.jrpc = jrpc;
 
   ws.on('message', function incoming(data) {
-	  // RPC serveri palauttaa ajan jolloin tätä functiota kutsutaan jolloin
-	  // lähetetään taas uusi kutsu --> luupi
   	  console.log('ws.on message:' + data);
       jrpc.messageHandler(data);
-  	  //jrpc.call('mul', {y: 3, x: 2}).then(function (result) {
-      //     console.log('>> mul result: ' + result);
-      //});
   });
 
   ws.jrpc.toStream = function(message){
@@ -31,11 +36,44 @@ exports.startWS = function () {
   }
 
   jrpc.on('_Keepalive', [], function(){
-    return "";
+    return {};
   });
-  ///////////////////////////////////////////
-};
 
+  jrpc.on('StatusEvent', ['timestamp', 
+						'ready_for_transaction', 
+						'psp_connection_available', 
+						'transaction_status', 
+						'chip_card_in',
+						'update_status',
+						'update_progress',
+						'update_eta',
+						'battery_charging',
+						'plugged_in'],
+						function(timestamp, 
+						ready_for_transaction, 
+						psp_connection_available, 
+						transaction_status, 
+						chip_card_in,
+						update_status,
+						update_progress,
+						update_eta,
+						battery_charging,
+						plugged_in) {
+	if (timestamp) {
+		console.log('timestamp ' + timestamp);
+	}
+	if (ready_for_transaction) {
+		console.log('ready_for_transaction ' + ready_for_transaction);
+	}
+	if (psp_connection_available) {
+		console.log('psp_connection_available ' + psp_connection_available);
+	}
+})
+
+jrpc.on('TerminalInfo', ['result'], function(result){
+  console.log('TerminalInfo: ' + result);
+});
+};
 
 exports.mul = function () {
   jrpc.call('mul', {y: 3, x: 2}).then(function (result) {
